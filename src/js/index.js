cd437e1fde4891d275b8151fed501e6f434e945f
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-08-26 19:21:57 +0900

    completed coffee app

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,13 @@
-//! step1 목표
-//  - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다
-//  - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
-//  - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-//  - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
-//  - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-//  - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
-
-// 메뉴 수정 TODO
-// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
-
-// 메뉴 삭제 TODO
-// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [x] 확인 버튼을 끌릭하면 메뉴가 삭제된다.
-// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
-
-//! step2 목표
-// - [ ] localStorage에 데이터를 저장하여 새로고침해도 데이터가 남아있게 한다.
-// - [ ] 에스프레소, 프라푸치노, 블렌디드, 티바나, 디저트 각각의 종류별로 메뉴판을 관리할 수 있게 만든다.
-// - [ ] 페이지에 최초로 접근할 때는 에스프레소 메뉴가 먼저 보이게 한다.
-// - [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-// - [ ] 품절 상태 메뉴의 마크업
-
-// TODO localStorage Read & Write
-// - [x] localStorage에 데이터를 저장한다
-// - [x] 메뉴를 추가할 때
-// - [x] 메뉴를 수정할 때
-// - [x] 메뉴를 삭제할 때
-// - [x] localStorage에 데이터를 읽어온다
-
-// TODO 카테고리별 메뉴판 정리
-// - [x] 에스프레소 메뉴판 정리
-// - [x] 프라푸치노 메뉴판 정리
-// - [x] 블렌디드 메뉴판 정리
-// - [x] 티바나 메뉴판 정리
-// - [x] 디저트 메뉴판 정리
-
-// TODO 페이지 접근시 최초 데이터 Read & Rendering
-// - [x] 페이지에 최초로 로딩될때 localStorage에 에스프레소 메뉴를 읽어온다.
-// - [x] 에스프레소 메뉴를 페이지에 그려준다.
-
-// TODO 품절 상태 관리
-// - [] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-// - [] 품절 버튼을 추가한다.
-// - [] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
-// - [] 클릭이벤트에서 가장가까운 li태그의 class속성 값에 sold-out을 추가한다.
-import store from "../store/index.js";
 import { $ } from "../utils/dom.js";
+import MenuApi from "../api/index.js";
+
+const formTag = $("#espresso-menu-form");
+const inputTag = $("#espresso-menu-name");
+const ulTag = $("#espresso-menu-list");
+const navTag = $("nav");
+const h2Tag = $("#category-title");
 
 function App() {
-  const formTag = $("#espresso-menu-form");
-  const inputTag = $("#espresso-menu-name");
-  const ulTag = $("#espresso-menu-list");
-  const navTag = $("nav");
-  const h2Tag = $("#category-title");
-
   // this를 이용한 변수 선언 (배열)
   this.menu = {
     espresso: [],
@@ -65,32 +18,43 @@ function App() {
   };
 
   this.currentCategory = "espresso";
-
+  console.log(this.menu, "?");
   // App 시작 시,즉시 init메서드가 호출되어 웹페이지가 렌더링된다.
-  this.init = (currentCategory) => {
+  this.init = async () => {
     // 로컬스토리지에 저장된 값을 찾는다.
-    console.log(currentCategory, "init");
-    if (store.getLocalStorage()) {
-      // 결과값을 menu에 할당한다.
-      this.menu = store.getLocalStorage();
+    console.log(this.currentCategory, "init");
 
-      // this.menu.push(...store.getLocalStorage());
-    }
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
+
+    // if (store.getLocalStorage()) {
+    // 결과값을 menu에 할당한다.
+    // this.menu = store.getLocalStorage();
+    // this.menu.push(...store.getLocalStorage());
+    // }
 
     // 렌더 함수를 호출하여 렌더링을 진행한다.
     render();
   };
 
-  const render = () => {
+  const render = async () => {
     // localStorage에 있는 데이터를 이용해서 렌더링
     // App의 메서드 init 함수는 this.menu에 값을 할당한다.
     // render함수는 this.menu로 menu에 접근 할 수 있다.
-    const template = this.menu[this.currentCategory]
-      ?.map((menuItem, index) => {
+
+    const data = await MenuApi.getAllMenuByCategory(this.currentCategory);
+
+    console.log(data);
+
+    const template = data
+      ?.map((menuItem) => {
         return `
-      <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+      <li data-menu-id="${
+        menuItem.id
+      }" class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name ${
-          menuItem.soldOut ? "sold-out" : ""
+          menuItem.isSoldOut ? "sold-out" : ""
         }">${menuItem.name}</span>
           <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button">
             품절
@@ -107,44 +71,57 @@ function App() {
 
     // ulTag를 기준으로 template가 아래에 렌더링된다.
     ulTag.innerHTML = template;
-    updateMenuCount();
+    updateMenuCount(data.length);
   };
 
   // 메뉴개수 표시 & input 초기화
-  const updateMenuCount = () => {
+  const updateMenuCount = (dataCount) => {
     // 상태관리에 맞게 이용하는것이 좋다. this.menu를 써라
-    const menuCount = this.menu[this.currentCategory].length;
+    // const menuCount = this.menu[this.currentCategory].length;
     // ulTag가 가지고 있는 모든 li의 길이를 구한다.
     // const menuCount = ulTag.querySelectorAll("li").length;
 
+    const menuCount = dataCount;
+
     // 변경된 개수를 새롭게 적용한다.
-    $(".menu-count").textContent = `총 ${menuCount}개`;
+    $(".menu-count").textContent = menuCount ? `총 ${menuCount}개` : "";
 
     // input 값을 초기화 시킨다.
     inputTag.value = "";
   };
 
   // 메뉴목록 추가
-  const addMenuName = () => {
+  const addMenuName = async () => {
     // 사용자가 입력한 input 값을 this.menu에 단순히 추가한다.
     if (inputTag.value === "") {
       alert("값을 입력해주세요");
       return;
     }
 
-    this.menu[this.currentCategory].push({ name: inputTag.value });
-    store.setLocalStorage(this.menu);
+    const duplicatedItem = this.menu[this.currentCategory].find(
+      (item) => item.name === inputTag.value
+    );
+
+    if (duplicatedItem) {
+      alert("이미 등록된 메뉴입니다. 다시 입력해주세요.");
+      console.log(duplicatedItem);
+      return;
+    }
+
+    // this.menu[this.currentCategory].push({ name: inputTag.value });
+    // store.setLocalStorage(this.menu);
     // this를 이용한 전역변수 menu를 로컬스토리지에 저장한다.
 
     // this.menu[categoryName].push({name:inputTag.value});
     // store.setLocalStorage(categoryName,this.menu[categoryName]);
+    await MenuApi.createMenu(this.currentCategory, inputTag.value);
 
     // 렌더 함수를 호출한다.
     render();
   };
 
   // 메뉴이름 변경
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     // 인덱스 반환
     const targetdLi = e.target.closest("li");
     const menuId = targetdLi.dataset.menuId;
@@ -155,40 +132,45 @@ function App() {
       $menuName.textContent
     );
 
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+
+    render();
+
     // index 반환, this.menu[0], this.menu[1] ...
     // 선택한 리스트의 이름에 프롬프트값을 넣어준다.
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
+    // this.menu[this.currentCategory][menuId].name = updatedMenuName;
 
     // 로컬스토리지에 변경사항 반영
-    store.setLocalStorage(this.menu);
+    // store.setLocalStorage(this.menu);
 
     // text 변경을 반영한다. render함수가 별도로 작동하지 않기 떄문에
     // $menuName.textContent = updatedMenuName;
 
     // render 함수로 대체
-    render();
   };
 
   // 메뉴목록 삭제
-  const removeMenuName = (e) => {
+  const removeMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
-    this.menu[this.currentCategory].splice(menuId, 1);
-    store.setLocalStorage(this.menu);
+
+    await MenuApi.deleteMenu(this.currentCategory, menuId);
+    // this.menu[this.currentCategory].splice(menuId, 1);
+    // store.setLocalStorage(this.menu);
 
     render();
   };
 
-  const soldOutMenu = (e) => {
+  const soldOutMenu = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
+    await MenuApi.toggleSoldOutMenu(this.currentCategory, menuId);
 
-    this.menu[this.currentCategory][menuId].soldOut =
-      !this.menu[this.currentCategory][menuId].soldOut;
-
-    store.setLocalStorage(this.menu);
     render();
+    // this.menu[this.currentCategory][menuId].soldOut =
+    // / !this.menu[this.currentCategory][menuId].soldOut;
+
+    // store.setLocalStorage(this.menu);
   };
 
-  const initEventListeners = () => {};
   // form태그가 자동으로 전송되는걸 막아준다.
   formTag.addEventListener("submit", (e) => e.preventDefault());
 
@@ -256,18 +238,25 @@ function App() {
     }
   });
 
-  navTag.addEventListener("click", (e) => {
+  const changeCategory = (e) => {
     const isCategoryButton = e.target.classList.contains("cafe-category-name");
 
     if (isCategoryButton) {
+      // nav 태그 클릭 시, name 저장
       const categoryName = e.target.dataset.categoryName;
+
+      // name 값 저장
       this.currentCategory = categoryName;
-      this.init(this.currentCategory);
+
+      // this로 상태값이 변경되어서 인자로 값을 넘겨줄 필요가 없다.
+      this.init();
 
       const categoryTitle = e.target.textContent;
       h2Tag.textContent = `${categoryTitle} 메뉴 관리`;
     }
-  });
+  };
+
+  navTag.addEventListener("click", changeCategory);
 }
 
 const app = new App();
---
+++ src/text.js
@@ -0,0 +1,170 @@
+//! step1 목표
+//  - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다
+//  - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+//  - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+//  - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+//  - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+//  - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+// 메뉴 수정 TODO
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+// 메뉴 삭제 TODO
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [x] 확인 버튼을 끌릭하면 메뉴가 삭제된다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
+//! step2 목표
+// - [x] localStorage에 데이터를 저장하여 새로고침해도 데이터가 남아있게 한다.
+// - [x] 에스프레소, 프라푸치노, 블렌디드, 티바나, 디저트 각각의 종류별로 메뉴판을 관리할 수 있게 만든다.
+// - [x] 페이지에 최초로 접근할 때는 에스프레소 메뉴가 먼저 보이게 한다.
+// - [x] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [x] 품절 상태 메뉴의 마크업
+
+// TODO localStorage Read & Write
+// - [x] localStorage에 데이터를 저장한다
+// - [x] 메뉴를 추가할 때
+// - [x] 메뉴를 수정할 때
+// - [x] 메뉴를 삭제할 때
+// - [x] localStorage에 데이터를 읽어온다
+
+// TODO 카테고리별 메뉴판 정리
+// - [x] 에스프레소 메뉴판 정리
+// - [x] 프라푸치노 메뉴판 정리
+// - [x] 블렌디드 메뉴판 정리
+// - [x] 티바나 메뉴판 정리
+// - [x] 디저트 메뉴판 정리
+
+// TODO 페이지 접근시 최초 데이터 Read & Rendering
+// - [x] 페이지에 최초로 로딩될때 localStorage에 에스프레소 메뉴를 읽어온다.
+// - [x] 에스프레소 메뉴를 페이지에 그려준다.
+
+// TODO 품절 상태 관리
+// - [x] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [x] 품절 버튼을 추가한다.
+// - [x] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+// - [x] 클릭이벤트에서 가장가까운 li태그의 class속성 값에 sold-out을 추가한다.
+
+//! step3 목표
+// TODO 서버 요청 부분
+// - [x] 웹 서버를 띄운다.
+// - [x] 서버에 새로운 메뉴명을 추가될 수 있도록 요청한다.
+// - [x] 서버에 카테고리별 메뉴리스트를 불러온다.
+// - [x] 서버에 메뉴가 수정 될 수 있도록 요청한다.
+// - [x] 서버에 메뉴의 품절상태를 토글될 수 있도록 한다.
+// - [x] 서버에 메뉴가 삭제 될 수 있도록 요청한다.
+
+// TODO 리팩터링 부분
+// - [x] localStorage에 저장하는 로직은 지운다.
+// - [x] fetch 비동기 api를 사용하는 부분을 async await를 사용하여 구현한다.
+
+// TODO 사용자 경험
+// - [x] API 통신이 실패하는 경우에 대해 사용자가 알 수 있게 alert으로 예외처리를 진행한다.
+// - [x] 중복되는 메뉴는 추가할 수 없다.
+
+// step3 회고
+// 1. 웹서버 띄우는것
+// 2. BASE_URL 웹 서버 변수 먼저 선언
+// 3. 비동기 처리하는데 해당하는 부분이 어디인지 확인하고, 웹서버에 요청하게끔 코드 짜기
+// 4. 서버에 요청한 후 데이터를 받아서 화면에 렌더링 하기
+// 5. 리팩터링
+// - localStorage 부분 지우기
+// - API 파일 따로 만들어서 진행
+// - 페이지 렌더링과 관련해서 중복되는 부분을 제
+// - 서버 요청 할 떄 option 객체
+// - 카테고리 버튼 클릭 시 콜백
+// 6. 사용자 경험 부분
+
+// ------------------------------------------------------------------------- //
+// ! 오늘 얻은 인사이트
+//? 1. 이벤트 위임을 어떻게 할 수 있는지 알게되어 좋았다.
+//? 2. 요구사항을 전략적으로 접근해야되는지, 단계별로 세세하게 나누는 것이 중요하다
+//? 3. DOM 요소를 가져올 때 $표시로 변수처럼 사용 할 수 있어서 좋았다.
+//? 4. contains, closest, textContent, insertAdjacentHtml, e.target
+
+//! step1 목표
+//  - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다
+//  - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+//  - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+//  - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+//  - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+//  - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+// 메뉴 수정 TODO
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+// 메뉴 삭제 TODO
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [x] 확인 버튼을 끌릭하면 메뉴가 삭제된다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
+//! 코드 주석 구분
+// todo 내 방법 - 클래스 이름으로 찾는 방법
+// const btn = e.target.className.split(" ").at(-1);
+// if (btn === "menu-edit-button") {
+//   console.log("수정버튼");
+// } else if (btn === "menu-remove-button") {
+//   console.log("제거버튼");
+// }
+
+// todo 내 방법 - 텍스트로 찾는방법
+// if (e.target.textContent === "수정") {
+//   console.log("수정버튼");
+// } else if (e.target.textContent === "삭제") {
+//   console.log("삭제버튼");
+// }
+
+// todo 내 방법 - 텍스트로 찾는방법
+// 부모 요소에서 자식요소로 접근하여 길이를 구하고 있다.
+//   const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+
+// innerText와 textContent 차이점 style과 리플로우 발생. innerText는 좋지않다.
+//   $(".menu-count").innerText = `총 ${menuCount}개`;
+
+//   나의 솔루션
+//   $(".menu-count").textContent = `총 ${_$(".menu-list-item").length}개`;
+
+//   $("#espresso-menu-name").value = "";
+
+// const menuItemTemplate = (espressoMenuName) => {
+//   return `<li class="menu-list-item d-flex items-center py-2">
+//               <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+//               <button
+//                 type="button"
+//                 class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+//               >
+//                 수정
+//               </button>
+//               <button
+//                 type="button"
+//                 class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+//               >
+//                 삭제
+//               </button>
+//             </li>`;
+// };
+
+// ulTag.insertAdjacentHTML("beforeend", menuItemTemplate(inputTag.value));
+
+// const subStore = {
+//   setSubLocalStorage(key, value) {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   },
+//   getSubLocalStorage(key) {
+//     return JSON.parse(localStorage.getItem(key));
+//   },
+// };
+
+//   this.subInit = (key) => {
+//     // 로컬스토리지에 저장된 값을 찾는다.
+//     if (subStore.getSubLocalStorage().length >= 1) {
+//       // 결과값을 menu에 할당한다.
+//       this.menu = subStore.getSubLocalStorage(key);
+//     }
+
+//     // 렌더 함수를 호출하여 렌더링을 진행한다.
+//     render();
+//     console.log("init이 호출되었는가??");
+//   };
